d6107d83d2fe68cd44dbbd361adeb2086e7aa262
JoaoTrento <trentojoao01@gmail.com>
2025-11-15 10:39:48 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -26,8 +26,6 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            double thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
 
@@ -40,17 +38,16 @@ public class Customer {
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                      String.valueOf(thisAmount) + "\n";
+                      String.valueOf(each.getCharge()) + "\n";
 
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
 
-        // add footer lines
+        // footer
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                   " frequent renter points";
 
         return result;
     }
-
 }
